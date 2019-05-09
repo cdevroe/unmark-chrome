aa1d78d72854f12a5ab864434c0dec6dbe5d4f50
Colin Devroe <colin@cdevroe.com>
2019-05-09 15:56:25 -0400

    Updated 16px icon. Updated manifest. Moved old CSS to unmark-ancient. Updated CSS to latest Unmark CSS. Updated font in header to Sources San Pro.

--- js/unmark/base.js
+++ js/unmark/base.js
@@ -1,5 +1,5 @@
 var unmark            = (unmark === undefined) ? {} : unmark;
-unmark.host           = 'https://unmark.it';
+unmark.host           = 'http://localhost';
 unmark.paths          = {
     'add'     : '/mark/add',
     'archive' : '/mark/archive',
